56793393affc95de329c479687b5ed027381a65d
lava m <lavanymkanna@gmail.com>
2016-10-03 11:39:38 -0400

    pushing to heroku

--- twitter_clone/wsgi.py
+++ twitter_clone/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_clone.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_clone.settings_local")
 
 application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangpWhiteNoise(application)
